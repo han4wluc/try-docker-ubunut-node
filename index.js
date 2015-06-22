f625c06d1d8a35a923b944e5492136612f2ef93e
Hanyon <han4wluc@gmail.com>
2015-06-22 16:03:20 +0000

    add mongoose

--- index.js
+++ index.js
@@ -9,5 +9,40 @@ app.get('/', function (req, res) {
   res.send('Hello world\n');
 });
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/node-mongo');
+mongoose.connection.once('open', function(callback){
+  console.log('mongoose connection opened at ' + 'mongodb://localhost/node-mongo');
+});
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', console.log);
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+  name: String
+});
+
+var User = mongoose.model('User', UserSchema);
+
+app.get('/users/add', function(req, res){
+  var name = req.param('name');
+  var user = new User({name:name});
+  user.save(function(err, savedUser){
+    if(err){
+      console.log(err);
+      res.send(err);
+    }
+    res.json(savedUser);
+  });
+});
+
+app.get('/users', function(req, res){
+  User.find().exec(function(err, users){
+    console.log(users);
+    res.json(users);
+  });
+});
+  
+
+
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
